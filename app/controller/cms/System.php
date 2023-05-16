e80e92159b2dc58c5ca96214f6dd1d6398df673c
zhangchunsheng <zhangchunsheng423@gmail.com>
2023-05-16 17:30:30 +0800

    update

--- app/controller/cms/System.php
+++ app/controller/cms/System.php
@@ -19,7 +19,7 @@ class System extends BaseController
         return app('json')->go($data);
     }
     //按类型获取配置信息
-    public function getConfigType()
+    public function getConfigType($type)
     {
         $data=SysConfigModel::where(['type'=>1,'is_use'=>1])->order('id asc')->
         field('id,key,value,desc,switch,other')->select();
